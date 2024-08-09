5aeba5849a826b73d5b70a3f83980ba6ffac6952
StashC <stashubc@student.ubc.ca>
2024-08-09 08:21:14 -0700

    move topbar items a bit closer

--- src/Components/TopBar/TopBar.tsx
+++ src/Components/TopBar/TopBar.tsx
@@ -11,27 +11,29 @@ const TopBar = () => {
 
   return (
     <div className="TopBar">
-      <h2 className="TopBarName">Stash Currie</h2>
-      <div className={`TopBarLinkContainer ${isOpen ? "Open" : ""}`}>
-        <a className="TopBarLink" onClick={() => scrollTo("PersonalProfile")}>
-          Home
-        </a>
-        <a className="TopBarLink" onClick={() => scrollTo("PersonalProfile")}>
-          About
-        </a>
-        <a className="TopBarLink" onClick={() => scrollTo("ProjectGallery")}>
-          Projects
-        </a>
-        <a
-          aria-label="Email"
-          className="TopBarLink"
-          onClick={() => scrollTo("ContactCard")}
-        >
-          Contact
-        </a>
-      </div>
-      <div className="NavToggle" onClick={toggleOpen}>
-        &#9776;
+      <div className="TopBarContentContainer">
+        <h2 className="TopBarName">Stash Currie</h2>
+        <div className={`TopBarLinkContainer ${isOpen ? "Open" : ""}`}>
+          <a className="TopBarLink" onClick={() => scrollTo("PersonalProfile")}>
+            Home
+          </a>
+          <a className="TopBarLink" onClick={() => scrollTo("PersonalProfile")}>
+            About
+          </a>
+          <a className="TopBarLink" onClick={() => scrollTo("ProjectGallery")}>
+            Projects
+          </a>
+          <a
+            aria-label="Email"
+            className="TopBarLink"
+            onClick={() => scrollTo("ContactCard")}
+          >
+            Contact
+          </a>
+        </div>
+        <div className="NavToggle" onClick={toggleOpen}>
+          &#9776;
+        </div>
       </div>
     </div>
   );
